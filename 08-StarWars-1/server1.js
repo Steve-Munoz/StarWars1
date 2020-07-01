bff052cff8fd8ac043561c12ab4ba31a41916e5f
Steve Munoz <stevomunoz@yahoo.com>
2020-06-30 19:46:20 -0500

    updated

--- 08-StarWars-1/server1.js
+++ 08-StarWars-1/server1.js
@@ -6,29 +6,35 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 // Data
-var characters = [
-  {
-    routeName: "yoda",
-    name: "Yoda",
-    role: "Jedi Master",
-    age: 900,
-    forcePoints: 2000,
-  },
-  {
-    routeName: "darthmaul",
-    name: "Darth Maul",
-    role: "Sith Lord",
-    age: 200,
-    forcePoints: 1200,
-  },
-  {
-    routeName: "obiwankenobi",
-    name: "Obi Wan Kenobi",
-    role: "Jedi Knight",
-    age: 60,
-    forcePoints: 1350,
-  },
-];
+// ===========================================================
+var yoda = {
+  name: "Yoda",
+  role: "Jedi Master",
+  age: 900,
+  forcePoints: 2000,
+};
+
+var darthmaul = {
+  name: "Darth Maul",
+  role: "Sith Lord",
+  age: 200,
+  forcePoints: 1200,
+};
+
+var obiWan = {
+  name: "Obi Wan Kenobi",
+  role: "Jedi Knight",
+  age: 30,
+  forcePoints: 3200,
+};
+
+// Create one more data entry for the character Obi Wan Kenobi.
+// Enter any values you like for the parameters following the same format as the Yoda and Darth Maul character
+//
+
+// YOUR CODE GOES HERE
+
+//
 
 // Routes
 // ===========================================================
@@ -36,14 +42,24 @@ app.get("/", function (req, res) {
   res.send("Welcome to the Star Wars Page!");
 });
 
-app.get("/:character", function (req, res) {
-  var chosen = req.params.character;
-
-  // What does this log?
-  console.log(chosen);
-
-  res.end();
+app.get("/yoda", function (req, res) {
+  res.json(yoda);
 });
+
+app.get("/darthmaul", function (req, res) {
+  res.json(darthmaul);
+});
+app.get("/obiWan", function (req, res) {
+  res.JSON(obiWan);
+});
+
+// Create a new Express route that leads users to the new Obi Wan Kenobi Data
+// Follow the same format as the Yoda and Darth Maul routes
+//
+
+// YOUR CODE GOES HERE
+//
+//
 
 // Listener
 // ===========================================================
